309aca6c3080e235ccbb2d694eb36714de5f4478
hi-im-na <lengocanh2k@gmail.com>
2024-12-26 17:22:07 +0700

    done #1444

--- src/components/modal/Criterias/AddCriteriasModal.vue
+++ src/components/modal/Criterias/AddCriteriasModal.vue
@@ -9,17 +9,15 @@
           </div>
 
           <div class="modal-body">
-            <form ref="criteriaForm" class="form" @submit.prevent="submitForm">
+            <form ref="criteriaForm" class="form">
               <div class="mb-3">
                 <label for="title" class="form-label">Tên tiêu chí</label>
-                <input type="text" class="form-control" id="title" v-model="criteria.title" required
-                  placeholder="Nhập tiêu chí đánh giá" />
+                <input type="text" class="form-control" id="title" v-model="criteria.title" @blur="validateTitle"
+                  :class="{ 'is-invalid': errors.title }" placeholder=" Nhập tiêu chí đánh giá" />
+                <div v-if="errors.title" class="invalid-feedback">
+                  {{ errors.title }}
+                </div>
               </div>
-              <!-- <div class="mb-3">
-                <label for="point" class="form-label">Số điểm</label>
-                <input type="text" class="form-control" id="point" v-model="criteria.point" required
-                  placeholder="Nhập số điểm" />
-              </div> -->
             </form>
           </div>
 
@@ -50,9 +48,12 @@ export default {
     return {
       criteria: {
         title: "",
-        point: ""
+      },
+      errors: {
+        title: null,
       },
       isLoading: false,
+      isFormFilled: false,
     };
   },
   methods: {
@@ -61,62 +62,82 @@ export default {
       this.$emit("close");
     },
     async addCriteria() {
-      this.isLoading = true;
-      const form = this.$refs.criteriaForm;
-      if (form.reportValidity()) {
-        const newCriteria = JSON.parse(JSON.stringify(this.criteria));
-        const formData = new FormData();
-        for (const key in newCriteria) {
-          // if (key === "avatar") continue;
-          formData.append(key, newCriteria[key]);
-        }
-        // formData.append("avatar", this.selectedImage);
+      this.validateForm();
+      if (Object.values(this.errors).every(x => x === null || x === '')) {
+        console.log("Form is valid");
+        this.isFormFilled = true;
+      }
+      if (!this.isFormFilled) {
+        return;
+      }
+      const newCriteria = JSON.parse(JSON.stringify(this.criteria));
+      const formData = new FormData();
+      for (const key in newCriteria) {
+        formData.append(key, newCriteria[key]);
+      }
 
-        try {
-          /**
-           * @type {ApiResponse}
-           */
-          const res = await CriteriasService.addCriteria(formData);
-          if (res.code) {
-            this.$emit("criteria-added");
-            Swal.fire({
-              title: res.message,
-              icon: "success",
-              timer: 1500,
-              showConfirmButton: false,
-            });
-
-            this.resetForm();
-            setTimeout(() => {
-              this.closeModal();
-            }, 1500);
-          }
-        } catch (error) {
-          console.error("Error adding criteria:", error);
+      try {
+        /**
+         * @type {ApiResponse}
+         */
+        const res = await CriteriasService.addCriteria(formData);
+        if (res.code) {
+          this.$emit("criteria-added");
           Swal.fire({
-            title: "Lỗi!",
-            text: error.response.data.error,
-            icon: "error",
-            confirmButtonText: "Đóng",
+            title: res.message,
+            icon: "success",
+            timer: 1500,
+            showConfirmButton: false,
           });
+
           this.resetForm();
           setTimeout(() => {
             this.closeModal();
           }, 1500);
-        } finally {
-          // Đảm bảo isLoading sẽ được đặt lại thành false
-          this.isLoading = false;
         }
+      } catch (error) {
+        console.error("Error adding criteria:", error);
+        Swal.fire({
+          title: "Lỗi!",
+          text: error.response.data.error,
+          icon: "error",
+          confirmButtonText: "Đóng",
+        });
+        this.resetForm();
+        setTimeout(() => {
+          this.closeModal();
+        }, 1500);
+      } finally {
+        // Đảm bảo isLoading sẽ được đặt lại thành false
+        this.isLoading = false;
       }
-    },
-    submitForm() {
-      const newCriteria = JSON.parse(JSON.stringify(this.criteria));
-      this.$emit("add-criteria", newCriteria);
     },
     resetForm() {
       this.criteria = {};
     },
+
+    validateForm() {
+      this.validateTitle();
+    },
+
+    validateTitle() {
+      if (!this.criteria.title) {
+        this.errors.title = "Vui lòng nhập tên tiêu chí";
+      } else {
+        this.errors.title = null;
+      }
+    },
+
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.is-invalid {
+  border-color: #dc3545;
+}
+
+.invalid-feedback {
+  color: #dc3545;
+  font-size: 0.875em;
+}
+</style>
