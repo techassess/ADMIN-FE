e53838915031a2c32bec157d1e4b87906b51cb5a
Y Âm <yam532004@gmail.com>
2025-01-08 15:59:31 +0700

    fix feature#1657

--- src/components/DetailUserRating.vue
+++ src/components/DetailUserRating.vue
@@ -36,88 +36,66 @@
   <br />
   <br />
   <div class="content">
-    <table class="table table-bordered table-light">
-      <thead>
-        <tr>
-          <th>
-            Hệ số
-            <MyTooltips
-              description="Số điểm mà 1 tiêu chí chiếm trong tổng số điểm đánh giá"
-            />
-          </th>
-          <th>Tiêu Chí</th>
-          <th>
-            Tự đánh giá
-            <MyTooltips
-              description="Số điểm trung bình mà bạn tự đánh giá được cho 1 tiêu chí trên thang 5"
-            />
-          </th>
-          <th>
-            Quản Lý
-            <MyTooltips
-              description="Số điểm trung bình mà quản lý đánh giá được cho 1 tiêu chí trên thang 5"
-            />
-          </th>
-          <th>
-            Team
-            <MyTooltips
-              description="Số điểm trung bình mà team đánh giá được cho 1 tiêu chí trên thang 5"
-            />
-          </th>
-          <th>
-            Tổng Điểm
-            <MyTooltips
-              description="Tổng số điểm mà 1 tiêu chí đạt được (dựa trên thang điểm của tiêu chí)"
-            />
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="(row, index) in overallRatedResDto.overallOfCriteria"
-          :key="overallRatedResDto.overallOfCriteria[index].criteriaId"
-        >
-          <td>{{ row.criteriaPoint }}</td>
-          <td>{{ row.criteriaTitle }}</td>
-          <td>{{ row.selfPoint }}</td>
-          <td>{{ row.managerPoint }}</td>
-          <td>{{ row.teamPoint }}</td>
-          <td>{{ row.overallPoint }}</td>
-        </tr>
-        <tr>
-          <td colspan="4" rowspan="3"></td>
-          <td>
-            Điểm đánh giá
-            <MyTooltips
-              description="Tổng số điểm mà bạn đạt được (trên thang điểm 100)"
-            />
-          </td>
-          <td>{{ overallRatedResDto.overallPoint }}</td>
-        </tr>
-        <tr>
-          <!-- <td colspan="4"></td> -->
-          <td>
-            Xếp loại
-            <MyTooltips
-              description="Xếp loại của bạn dựa trên tổng số điểm đánh giá"
-            />
-          </td>
-          <td>{{ overallRatedResDto.rank }}</td>
-        </tr>
-        <tr>
-          <!-- <td colspan="4"></td> -->
-          <td>
-            Đề xuất tăng bậc
-            <MyTooltips
-              description="Đề xuất tăng bậc của bạn dựa trên xếp loại"
-            />
-          </td>
-          <td>{{ overallRatedResDto.levelUpRecommend }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="mt-2">
+      <table class="table table-hover table-bordered my-table">
+        <thead>
+          <tr>
 
-    <RadarChart />
+            <th>Tiêu Chí</th>
+            <th>Tự đánh giá
+              <MyTooltips description="Số điểm trung bình mà bạn tự đánh giá được cho 1 tiêu chí trên thang 5" />
+            </th>
+            <th>Quản Lý
+              <MyTooltips description="Số điểm trung bình mà quản lý đánh giá được cho 1 tiêu chí trên thang 5" />
+            </th>
+            <th>Team
+              <MyTooltips description="Số điểm trung bình mà team đánh giá được cho 1 tiêu chí trên thang 5" />
+            </th>
+            <th>Điểm đạt được
+              <MyTooltips description="Tổng số điểm mà 1 tiêu chí đạt được (dựa trên thang điểm của tiêu chí)" />
+            </th>
+            <th>Hệ số
+              <MyTooltips description="Số điểm mà 1 tiêu chí chiếm trong tổng số điểm đánh giá" />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, index) in overallRatedResDto.overallOfCriteria"
+            :key="overallRatedResDto.overallOfCriteria[index].criteriaId">
+            <td style="font-weight: 600;">{{ row.criteriaTitle }}</td>
+            <td>{{ row.selfPoint }}</td>
+            <td>{{ row.managerPoint }}</td>
+            <td>{{ row.teamPoint }}</td>
+            <td>{{ row.overallPoint }}</td>
+            <td>{{ row.criteriaPoint }}</td>
+          </tr>
+          <tr>
+            <td colspan="6" class="bg-warning" style="font-weight: 600; color: black;">Tổng hợp</td>
+          </tr>
+          <tr>
+            <td colspan="4"></td>
+            <td>Điểm đánh giá (thang điểm 100)
+              <MyTooltips description="Tổng số điểm mà bạn đạt được (trên thang điểm 100)" />
+            </td>
+            <td>{{ overallRatedResDto.overallPoint }}</td>
+          </tr>
+          <tr>
+            <td colspan="4"></td>
+            <td>Xếp loại
+              <MyTooltips description="Xếp loại của bạn dựa trên tổng số điểm đánh giá" />
+            </td>
+            <td>{{ overallRatedResDto.rank }}</td>
+          </tr>
+          <tr>
+            <td colspan="4"></td>
+            <td>Đề xuất tăng bậc
+              <MyTooltips description="Đề xuất tăng bậc của bạn dựa trên xếp loại" />
+            </td>
+            <td>{{ overallRatedResDto.levelUpRecommend }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
   <RadarChart />
 
