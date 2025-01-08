47f9ef3e9b76fc28f67ee9f427268e5c3bb72ca8
Na <46052990+hi-im-na@users.noreply.github.com>
2025-01-08 10:26:21 +0700

    Feature/1097 1657 employee rating info (#34)
    * add criteria
    
    * add detail user rating page
    
    ---------
    
    Co-authored-by: Hoàng <hoanghung20030@gmail.com>

---
+++ src/components/common/MyTooltips.vue
@@ -0,0 +1,15 @@
+<template>
+    <a href="#" class="my-tooltip" data-bs-toggle="tooltip" data-bs-placement="top" :title="description">
+        <img src="../../assets/question.png" alt="" height="16px" width="16px">
+    </a>
+</template>
+
+<script>
+export default {
+    name: 'MyTooltips',
+    props: ['description'],
+    setup(props) {
+        console.log(props.description);
+    }
+}
+</script>
