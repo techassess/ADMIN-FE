ecb0f72e9e1da7f6828a71cf27da4d0c2c0a8eea
Na <46052990+hi-im-na@users.noreply.github.com>
2025-01-10 08:53:12 +0700

    Feature/1097 1757 handle add update employee (#38)
    * add criteria
    
    * done #1757
    
    ---------
    
    Co-authored-by: Hoàng <hoanghung20030@gmail.com>

---
+++ src/model/enum/EPosition.js
@@ -0,0 +1,11 @@
+const EPosition = Object.freeze({
+    FRESHER: 'Fresher',
+    JUNIOR: 'Junior',
+    MIDDLE: 'Middle',
+    SENIOR: 'Senior',
+    LEADER: 'Leader',
+    MANAGER: 'Manager',
+    DIRECTOR: 'Director',
+});
+
+export default EPosition;
